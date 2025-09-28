3e7dcfee10c591bc65420e21617b9bd1e39d2167
shriram-cmyk <shriram@vidocto.com>
2025-09-28 17:43:15 +0530

    New changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const sgTransport = require("nodemailer-sendgrid-transport");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
